25464de48c6063eccb96289a79d70692c7d7d77d
Yoram Cohen <yoram@phenota.com>
2024-10-06 12:14:38 +0300

    Change App name

--- anylabeling/app_info.py
+++ anylabeling/app_info.py
@@ -1,4 +1,4 @@
-__appname__ = "X-AnyLabeling"
+__appname__ = "PhenoTA Image Labeling Tool"
 __appdescription__ = "Advanced Auto Labeling Solution with Added Features"
 __version__ = "2.4.3"
 __preferred_device__ = "CPU"  # GPU or CPU
